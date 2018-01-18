55951751949353e20042e763d9e591caab6f69d9
muhammadsaripuddin <aripedward339@gmail.com>
2018-01-18 14:51:37 +0700

    kedua

--- aaaaaa.cpp
+++ aaaaaa.cpp
@@ -9,3 +9,15 @@ class remote{
 };
 
 int main(){
+	remote TV,AC,kipas;
+	
+	AC.harga = 20000;
+	kipas.harga = 40000;
+	TV.harga = 34000;
+	
+	cout <<"Harga Remote AC adalah Rp. "<<(TV.harga)<<endl;
+	cout <<"Harga Remote kipas adalah Rp. "<<(AC.harga)<<endl;
+	cout <<"Harga Remote TV adalah Rp. "<<(kipas.harga)<<endl;
+	
+	AC.fungsi("untuk menggunakan AC");
+}
